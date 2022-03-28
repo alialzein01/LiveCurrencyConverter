eb8c848f6dbed827bdcd514e2740657537bec968
Ali Alzein <aly.alzein01@gmail.com>
2022-03-28 18:16:59 +0300

    adding notes

--- app/src/main/java/com/example/group_project/calcActivity.java
+++ app/src/main/java/com/example/group_project/calcActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import java.text.DecimalFormat;
 
 public class calcActivity extends AppCompatActivity {
-
+    // declaring global variables
     EditText l_amount;
     EditText d_amount;
     Button calc;
@@ -22,6 +22,7 @@ public class calcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calcpage);
+        // linking the variables to the xml by id
         l_amount = (EditText) findViewById(R.id.lira_amount);
         d_amount = (EditText) findViewById(R.id.dollar_amount);
         calc = (Button) findViewById(R.id.convert_btn);
@@ -30,18 +31,22 @@ public class calcActivity extends AppCompatActivity {
     public void conv(View view) {
         String ll = l_amount.getText().toString();
         String dd = d_amount.getText().toString();
+        //closing the keyboard when the user press convert button
         closeKeyboard();
-        if(ll.isEmpty() && dd.isEmpty()){
+        // showing an error if the user did not fill any value
+        if (ll.isEmpty() && dd.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Please enter a value in one of the boxes", Toast.LENGTH_LONG).show();
         }
     }
 
     private String formatNum(String n) {
+        // this function gives a format to the results
         DecimalFormat form = new DecimalFormat("###,###,##0.0");
         return form.format(Double.parseDouble(n));
     }
 
     private void closeKeyboard() {
+        // the function used to close the keyboard
         View v = this.getCurrentFocus();
         if (v != null) {
             InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
