31df20bb282a5f0a5305d54e05a7f641a6278c45
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-03-30 18:44:25 +0300

    Editing Home XML to add refresh btn

--- app/src/main/java/com/example/group_project/MainActivity.java
+++ app/src/main/java/com/example/group_project/MainActivity.java
@@ -5,18 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView refresh_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+
+        refresh_button = findViewById(R.id.refresh_btn);
+        refresh_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(getIntent());
+            }
+        });
     }
 
     public void nextP(View view) {
         // Use the intent to move from one page to another, here from home page to calculator page
         Intent in = new Intent(MainActivity.this, calcActivity.class);
         startActivity(in);
+    }
+
+    public void refresh(View view){
+
     }
 }
