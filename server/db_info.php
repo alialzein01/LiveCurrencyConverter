e592390ad1119c7d2f1fb03846f9a9452cf7d65b
Ali Alzein <aly.alzein01@gmail.com>
2022-03-31 18:43:53 +0300

    Pushing Backend Folder

---
+++ server/db_info.php
@@ -0,0 +1,29 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = null;
+$database_name = "alihadi_db";
+
+$mysqli = new mysqli($server, $username, $password, $database_name);
+
+if(mysqli_connect_errno()){
+    die("Conenction Failed!");
+}
+
+$id = $_POST["id"];
+$live_rate = $_POST["live_rate"];
+$usd_rate = $_POST["usd_rate"];
+$lbp_rate = $_POST["lbp_rate"]
+
+$query = $mysqli->prepare("INSERT INTO historyofrate (id, live_rate, usd_rate,lbp_rate) VALUES (?, ?, ?,?)");
+$query->bind_param("sss", $id, $live_rate, $usd_rate,$lbp_rate);
+$query->execute();
+
+$response = [];
+$response["status"] = "Mabrouk!";
+
+$json_response = json_encode($response);
+echo $json_response;
+
+?>
