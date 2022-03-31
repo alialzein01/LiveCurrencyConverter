a2d9c962862a49ebc5d7076eabfa3377c6948212
Ali Alzein <aly.alzein01@gmail.com>
2022-03-31 15:03:21 +0300

    Adding Backend

--- frontend/app/src/main/java/com/example/group_project/MainActivity.java
+++ frontend/app/src/main/java/com/example/group_project/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.group_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    String url = "https://10.2.0.0/apiReq.php";
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ server/queries.php
@@ -0,0 +1,7 @@
+<?php
+
+include("db_info.php");
+
+
+
+?>
