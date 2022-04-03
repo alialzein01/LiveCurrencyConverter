d76bc48c458c79b5201cbea46af30ddc33924e83
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-04 01:51:06 +0300

    Adding functions to API2

--- app/src/main/java/com/example/group_project/calcActivity.java
+++ app/src/main/java/com/example/group_project/calcActivity.java
@@ -12,6 +12,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -25,6 +27,9 @@ public class calcActivity extends AppCompatActivity {
     Button calc;
     int buy_cal;
     int sell_cal;
+    String api_rate = null;
+    String to_display = null;
+    EditText editText;
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -82,15 +87,66 @@ public class calcActivity extends AppCompatActivity {
         sell_cal = Integer.parseInt(x.getStringExtra("sell_rate"));
     }
 
+    public class StoreRate extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls){
+            String result = "";
+            URL url;
+            HttpURLConnection http;
+
+            try{
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+            return result;
+        }
+
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try{
+                JSONObject json = new JSONObject(s);
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
     // Convert function
     public void conv(View view) {
+
         String ll = l_amount.getText().toString();
         String dd = d_amount.getText().toString();
         double buy_rate = buy_cal;
         double sell_rate = sell_cal;
-        boolean first_rate;
-        boolean second_rate;
-        double amount;
+        double first_rate = 0;
+        double second_rate = 0;
+        double amount = 0;
+
+
+        double x = Double.parseDouble(editText.getText().toString());
+        double rate = Double.parseDouble(api_rate);
+        String url = null;
+
+
+
 
         // showing an error if the user did not fill any value
         if (ll.isEmpty() && dd.isEmpty()) {
@@ -99,19 +155,16 @@ public class calcActivity extends AppCompatActivity {
         else if (!ll.isEmpty() && !dd.isEmpty()) {
             Toast.makeText(getApplicationContext(), "Fill only ONE Box", Toast.LENGTH_LONG).show();
         } else{
-            if(dd.isEmpty()) {
-                first_rate = true;
-                second_rate = false;
-                amount = Double.parseDouble(ll);
-                String dollar_s = String.valueOf(amount);
-            } else {
-                second_rate = true;
-                first_rate = false;
-                amount = Double.parseDouble(dd);
-                String lira_s = String.valueOf(amount);
+            if(ll.isEmpty()){
+                 url = "http://10.0.2.2/api2/?buy_rate="+buy_rate+"&sell_rate="+sell_rate+"&first_rate="+first_rate+"&second_rate="+second_rate+"&amount="+amount;
             }
-            String url2 = "http://10.0.2.2/api2/?buy_rate="+buy_rate+"&sell_rate="+sell_rate+"&first_rate="+first_rate+"&second_rate="+second_rate+"&amount="+amount;
+            else if (dd.isEmpty()){
+                url = "http://10.0.2.2/api2/?buy_rate="+buy_rate+"&sell_rate="+sell_rate+"&first_rate="+first_rate+"&second_rate="+second_rate+"&amount="+amount;;
+            }
+
         }
+        StoreRate storeRate = new StoreRate();
+        storeRate.execute(url);
     }
 
     //intent to go back to the rate page (home page)
