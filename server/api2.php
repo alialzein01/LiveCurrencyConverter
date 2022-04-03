08e353f4afaca647fce92d232380c0ecd57181b0
Ali Alzein <aly.alzein01@gmail.com>
2022-04-03 23:54:41 +0300

    fixing in api2

--- server/api2.php
+++ server/api2.php
@@ -3,7 +3,8 @@
 include ("db_info.php");
 
 // getting the values from android studio
-$live_rate = $_GET["live_rate"];
+$buy_rate = $_GET["buy_rate"];
+$sell_rate = $_GET["sell_rate"];
 $first_rate = $_GET["first_rate"];
 $second_rate = $_GET["second_rate"];
 $amount_converted = $_GET["amount"];
@@ -15,13 +16,13 @@ $ll_res = 0;
 
 // conversion in backend
 if($first_rate == "LL") {
-    $usd_res = $amount_converted / $live_rate;
+    $usd_res = $amount_converted / $buy_rate;
 } else {
-    $ll_res = $amount_converted * $live_rate;
+    $ll_res = $amount_converted * $sell_rate;
 }
 
 // inserting to db
-$query = $mysqli->prepare("INSERT INTO historyofrate (live_rate, first_rate,second_rate,amount_converted) VALUES ($live_rate, $first_rate, $second_rate,$amount_converted)");
+$query = $mysqli->prepare("INSERT INTO historyofrate (buy_rate,sell_rate, first_rate,second_rate,amount_converted) VALUES ($buy_rate,$sell_rate, $first_rate, $second_rate,$amount_converted)");
 $query->execute();
 
 echo json_encode($usd_res). " ".json_encode($ll_res);
