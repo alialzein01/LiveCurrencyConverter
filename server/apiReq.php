bd42d65c78e8acae7b1c9abc733aa8203cda4542
Ali Alzein <aly.alzein01@gmail.com>
2022-04-03 21:49:16 +0300

    changing in php

--- server/apiReq.php
+++ server/apiReq.php
@@ -1,16 +1,18 @@
 <?php
 
-$url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t202233118";
+// link of the api
+$url = "https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP&_ver=t20224221";
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_URL, $url);
 $resp = curl_exec($curl);
 $decoded = json_decode($resp,true);
 
+// getting the buy value
 $sizeArr = sizeof($decoded['buy']);
 $buy = $decoded['buy'][$sizeArr-1]["1"];
 echo $buy;
-
+// getting the sell value
 $sizeArr2 = sizeof($decoded['sell']);
 $sell = $decoded['sell'][$sizeArr2-1]["1"];
 echo (" ");
--- server/db_info.php
+++ server/db_info.php
@@ -5,6 +5,7 @@ $username = "root";
 $password = null;
 $database_name = "alihadi_db";
 
+//connecting the local database and initializing it
 $mysqli = new mysqli($server, $username, $password, $database_name);
 
 if(mysqli_connect_errno()){
